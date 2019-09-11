9d5a6d028808d09b19ae26043bf4e826cb0ca80b
i-lab user <i-labuser@SDPL88534-iMacProL.local>
2019-09-10 17:53:09 -0700

    robot grafitti

--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -13,8 +13,73 @@ public class RobotSquare {
  
     	// 1. Make a new Robot
 
+Robot awesome = new Robot(); 
+awesome.setSpeed(200);
+awesome.penDown();
+awesome.setRandomPenColor();
+awesome.turn(9);
+awesome.turn(9);
+awesome.move(200);
+awesome.setRandomPenColor();
+awesome.turn(9);
+awesome.turn(9);
+awesome.move(200);
+awesome.setRandomPenColor();
+awesome.turn(9);
+awesome.turn(9);
+awesome.move(200);
+awesome.setRandomPenColor();
+awesome.turn(9);
+awesome.turn(9);
+awesome.move(200);
+awesome.setRandomPenColor();
+awesome.turn(18);
+awesome.turn(18);
+awesome.move(400);
+awesome.setRandomPenColor();
+awesome.turn(18);
+awesome.turn(18);
+awesome.move(200);
+awesome.setRandomPenColor();
+awesome.turn(9);
+awesome.turn(9);
+awesome.move(100);
+awesome.setRandomPenColor();
+awesome.turn(45);
+awesome.turn(45);
+awesome.move(300);
 
-        // 3. Put the robot's pen down
+Robot hi = new Robot();
+hi.setX(700);
+hi.setSpeed(200);
+hi.penDown();
+hi.setRandomPenColor();
+hi.turn(90);
+hi.move(200);
+hi.setRandomPenColor();
+hi.turn(90);
+hi.move(200);
+hi.setRandomPenColor();
+hi.turn(90);
+hi.move(200);
+hi.setRandomPenColor();
+hi.turn(90);
+hi.move(200);
+hi.setRandomPenColor();
+hi.turn(180);
+hi.move(400);
+hi.setRandomPenColor();
+hi.turn(180);
+hi.move(200);
+hi.setRandomPenColor();
+hi.turn(90);
+hi.move(100);
+hi.setRandomPenColor();
+hi.turn(45);
+hi.move(300);
+
+
+// 3. Put the robot's pen down
 
 
         // 6. Make the robot move as fast as possible
