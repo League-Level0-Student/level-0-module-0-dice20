a5f5d5b0a4266dce5e347f207ffd97e64e107c38
dice20 <rodrigo.hernandez@jointheleague.org>
2019-09-24 17:51:50 -0700

    everythingisawesome

---
+++ src/EverythingIsAwesome.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class EverythingIsAwesome {
+	public static void main(String[] args) {
+		
+	
+		
+		
+		int happy = 0;
+		while  (happy < 5){
+			String input = JOptionPane.showInputDialog("What is awesome?");
+			JOptionPane.showMessageDialog(null,input+" is awesome");
+		happy += 1;
+		}
+		
+		}
+	}
+	
+	
+		
+		
+	
+	
---
+++ src/LastSummer.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class LastSummer {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "I know that you went camping last summer, rodrigo. Muhahaha");
+	
+	
+	
+	}
+
+}
